eb2fb080bdd9ca9ef423ae82e7a81104ba78e4cd
thinking_mo <split@live.cn>
2018-01-10 23:14:28 +0800

    [KSX] 分销商 静态页面搭建

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -12,6 +12,14 @@ import { DistributorOrderComponent } from './component/distributor-order/distrib
 import { DistributorCustomerComponent } from './component/distributor-customer/distributor-customer.component';
 import { DistributorGoodsComponent } from './component/distributor-goods/distributor-goods.component';
 
+
+// 供应商 路由
+import { SupplierGoodsCategoryComponent } from './component/supplier-goods-category/supplier-goods-category.component';
+import { SupplierDistributorLevelComponent } from './component/supplier-distributor-level/supplier-distributor-level.component';
+import { SupplierGoodsComponent } from './component/supplier-goods/supplier-goods.component';
+import { SupplierDistributorComponent } from './component/supplier-distributor/supplier-distributor.component';
+import { SupplierOrderComponent } from './component/supplier-order/supplier-order.component';
+
 export const ROUTES: Routes = [
   { path: '', component: AuthorizationComponent },
   { path: 'v_login', component: AuthorizationComponent }
@@ -24,4 +32,11 @@ export const ROUTES: Routes = [
   { path: 'distributor/v_order', component: DistributorOrderComponent },
   { path: 'distributor/v_customer', component: DistributorCustomerComponent },
   { path: 'distributor/v_goods', component: DistributorGoodsComponent },
+
+  { path: 'supplier/v_order', component: SupplierOrderComponent },
+  { path: 'supplier/v_distributor', component: SupplierDistributorComponent },
+  { path: 'supplier/v_goods', component: SupplierGoodsComponent },
+  { path: 'supplier/v_level', component: SupplierDistributorLevelComponent },
+  { path: 'supplier/v_category', component: SupplierGoodsCategoryComponent },
+
 ];
